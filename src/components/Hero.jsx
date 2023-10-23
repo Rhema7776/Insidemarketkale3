806b58c78bfa82a5e822d738b8f3c83c2b6519d6
Rhema7776 <rhemachukwumah7@gmail.com>
2023-10-23 20:18:19 +0100

    fifth commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,123 +6,263 @@ import playstore from "../images/Mobile app store badge.png";
 import appstore from "../images/Mobile app store badge (1).png";
 import { Link } from "react-router-dom";
 
-function App() {
-  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+const Hero = () => {
+  const [toggleState, setToggleState] = useState(1);
 
-  const handleTabChange = (e, tabIndex) => {
-    
-    setCurrentTabIndex(tabIndex);
-
+  const toggleTab = (index) => {
+    setToggleState(index);
   };
 
+ 
+  const data = [
+    {
+      label: "",
+      value: "html",
+      Header: "For buyers",
+      desc: `Post what you want to buy, whether in text, images or voice`,
+      img: require("../images/Content (3).png")
+    },
+    {
+      label: "",
+      value: "react",
+      Header: "For sellers and service providers",
+      desc: `Within minutes, sellers Respond to your posts and give you offers `,
+      img: require("../images/Content (4).png")
+    },
+    
+  ];
+
   return (
-    <React.Fragment className=" ">
-     
-
-      {/* TAB 1 Contents */}
-      {currentTabIndex === 0 && (
-        <Box className="container flex flex-wrap   mx-auto  md:flex-row ">
-          <Box sx={{ p: 0 }} className="lg:w-1/2">
-            <Tabs
-                className="container mx-auto mt-10 "
-                value={currentTabIndex}
-                onChange={handleTabChange}
-                variant="scrollable"
-                scrollButtons="auto"
-                aria-label="scrollable auto tabs example"
-                TabIndicatorProps={{
-                    sx: {backgroundColor: '#FF6A00', height: 4, color: "#FF6A00", 
-                  }
-                }}
-                InputLabelProps={{
-                  className: "textField__label"
-                  
-                }}
-            
-                start>
-                
-                <Tab
-                 className="text-xs " sx={{ textTransform: "none" }} label="For buyers" />
-                <Tab className="text-xs" sx={{ textTransform: "none", }} label="For Sellers and Service Providers" /> 
-            </Tabs>
-            
-            <h2
-              className="max-w-xl text-2xl font-extrabold leading-none text-center lg:text-5xl lg:text-left lg:leading-tight pb-5"
+    <Box id="" className="py-13">
+      <Box className="container mx-auto grid    md:grid-cols-2 flex-col justify-between  xs:max-lg:px-3  ">
+        <Box className="">
+          {/* Tab Indicators */}
+          <Box className=" border-b-2 mt-20  flex flex-wrap   border-white-500 xs:max-sm:max-w-[90%] sm:max-md:max-w-[43%] md:max-lg:max-w-[72%] lg:max-xl:max-w-[60%] xl:max-w-[48%] pb-3">
+            <Box
+              className={
+                toggleState === 1
+                  ? "tab relative top-[13px] border-orange-500 border-b-2  border-solid  xl:pt-0  "
+                  : "tab   border-white-500 pt-3 "
+              }
+              onMouseEnter={() => toggleTab(1)}
             >
-              Quickest Route to Connect with Sellers and Service Providers
-            </h2>
-            <Typography variant="p" className=" text-sm font-thin text-center text-gray-400 lg:text-left lg:max-w-md pb-5">
-              Are you in the market for a specific product or service, but don't
-              know where to start looking?InsideMarket is here to simplify your
-              search and make your buying and service-seeking experience
-              hassle-free.
-            </Typography>
-            <Box className="flex  mt-10  md:flex">
-              <Link to="/" className=" ">
-                <button className="border-none mr-4">
-                  <img src={appstore} alt="" />
-                </button>
-              </Link>
-              <Link to="/">
-                <button className="">
-                  <img src={playstore} alt="" />
-                </button>
-              </Link>
+              <p as="p" className=" text-sm text-gray-700  xs:max-lg:text-xs hover:text-orange-500 font-bold ">For buyers</p>
+            </Box>
+            <Box
+              className={
+                toggleState === 2
+                  ? "tab relative top-[13px]  border-orange-500 border-b-2 text-center border-solid pt-0"
+                  : "tab  border-orange-500  text-center pt-3"
+              }
+              onMouseEnter={() => toggleTab(2)}
+            >
+              <p as="p" className=" text-sm xs:max-lg:text-xs hover:text-orange-500 ml-5 font-bold text-gray-700">For sellers and service providers</p>
+              
             </Box>
           </Box>
 
-          <img className="lg:w-1/2" src={iphone1} alt="" />
-        </Box>
-      )}
-
-      {/* TAB 2 Contents */}
-      {currentTabIndex === 1 && (
-        <Box className="container flex flex-wrap justify-center mx-auto  md:flex-row ">
-            
-          <Box sx={{ p: 0 }} className="lg:w-1/2">
-
-            <Tabs
-                className=" container mx-auto mt-10  itesms-center lowercase "
-                value={currentTabIndex}
-                onChange={handleTabChange}
-                variant="scrollable"
-                scrollButtons="auto"
-                aria-label="scrollable auto tabs example"
-                TabIndicatorProps={{
-                    sx: {backgroundColor:  '#FF6A00', height: 4, textTransform: 'capitalize'}
-                }}
-                start>
-                <Tab className="text-xs capitalize" sx={{ textTransform: "none" }} label="For buyers" />
-                <Tab className="text-xs capitalize" sx={{ textTransform: "none" }} label="For Sellers and Service Providers" />
-            </Tabs>
-            
-            <h2 className="max-w-xl text-2xl font-extrabold leading-none text-center lg:text-5xl lg:text-left lg:leading-tight pb-5">
-              Connect with customers, increase your profits
-            </h2>
-            <Typography variant="p text-sm text-gray-500">
-              InsideMarket is your go-to destination to find ready-to-buy,
-              Ready-to-pay customers for your product or service. Install the
-              app to get started!
-            </Typography>
-            <Box className="flex  mt-10  md:flex">
-              <Link to="/" className=" ">
-                <button className="border-none mr-4">
+          <Box className="content-tab  items-center mt-4">
+            <Box
+              className={
+                toggleState === 1
+                  ? "content active-content "
+                  : "content items-center  "
+              }
+            >
+              <h1 className="max-w-xl text-2xl text-start font-extrabold leading-none  lg:text-5xl lg:text-left lg:leading-tight pb-5">
+                Quickest Route to Connect with Sellers and Service Providers
+              </h1>
+              <Typography variant="p" className=" text-sm font-thin  text-gray-400 lg:text-left lg:max-w-md pb-5">
+                Are you in the market for a specific product or service, but don't
+                know where to start looking?InsideMarket is here to simplify your
+                search and make your buying and service-seeking experience hassle-free.
+              </Typography>
+              <Box className="flex  mt-10  md:flex">
+                <Link to="/" className=" ">
+                  <button className="border-none mr-4">
                   <img src={appstore} alt="" />
-                </button>
-              </Link>
-              <Link to="/">
-                <button className="">
+                  </button>
+                </Link>
+                <Link to="/">
+                  <button className="">
                   <img src={playstore} alt="" />
-                </button>
-              </Link>
+                  </button>
+                </Link>
+              </Box>              
             </Box>
+            <Box
+              className={
+                toggleState === 2
+                  ? "content active-content"
+                  : "content items-center "
+              }
+            >
+              <h1 className="max-w-xl text-2xl font-extrabold leading-none lg:text-5xl lg:text-left lg:leading-tight pb-5">
+                Connect with customers, increase your profits
+              </h1>
+              <Typography variant="p text-sm text-gray-500">
+                InsideMarket is your go-to destination to find ready-to-buy,
+                Ready-to-pay customers for your product or service. Install the
+                app to get started!
+              </Typography>
+              <Box className="flex  mt-10  md:flex">
+                <Link to="/" className=" ">
+                  <button className="border-none mr-4">
+                  <img src={appstore} alt="" />
+                  </button>
+                </Link>
+                <Link to="/">
+                  <button className="">
+                  <img src={playstore} alt="" />
+                  </button>
+                </Link>
+              </Box> 
+            </Box>
+            
+            
           </Box>
-          
-          <img className="lg:w-1/2 " src={iphone2} alt="" />
         </Box>
-      )}
-    </React.Fragment>
+
+        
+        <div className="content-tab  items-center ">
+        <div
+          className={
+            toggleState === 1
+              ? "content active-content border-orange-500 border-t-6 "
+              : "content "
+          }
+        >
+          <img  src={iphone1} alt="" className="w-full " />
+        </div>
+        <div
+          className={toggleState === 2 ? "content active-content  " : "content   "}
+        >
+          <img src={iphone2} alt="" className="w-full" />
+        </div>
+      </div>
+      </Box>
+    </Box>
   );
-}
+};
 
-export default App;
+export default Hero;
+
+// function App() {
+//   const [currentTabIndex, setCurrentTabIndex] = useState(0);
+
+//   const handleTabChange = (e, tabIndex) => {
+
+//     setCurrentTabIndex(tabIndex);
+
+//   };
+
+//   return (
+//     <React.Fragment className=" ">
+
+//       {/* TAB 1 Contents */}
+//       {currentTabIndex === 0 && (
+//         <Box className="container flex flex-wrap   mx-auto  md:flex-row ">
+//           <Box sx={{ p: 0 }} className="lg:w-1/2">
+//             <Tabs
+//                 className="container mx-auto mt-10 "
+//                 value={currentTabIndex}
+//                 onChange={handleTabChange}
+//                 variant="scrollable"
+//                 scrollButtons="auto"
+//                 aria-label="scrollable auto tab example"
+//                 TabIndicatorProps={{
+//                     sx: {backgroundColor: '#FF6A00', height: 4, color: "#FF6A00",
+//                   }
+//                 }}
+//                 InputLabelProps={{
+//                   className: "textField__label"
+
+//                 }}
+
+//                 start>
+
+//                 <Tab
+//                  className="text-xs " sx={{ textTransform: "none" }} label="For buyers" />
+//                 <Tab className="text-xs" sx={{ textTransform: "none", }} label="For Sellers and Service Providers" />
+//             </Tabs>
+
+//             <h2
+//               className="max-w-xl text-2xl font-extrabold leading-none text-center lg:text-5xl lg:text-left lg:leading-tight pb-5"
+//             >
+//               Quickest Route to Connect with Sellers and Service Providers
+//             </h2>
+//             <Typography variant="p" className=" text-sm font-thin text-center text-gray-400 lg:text-left lg:max-w-md pb-5">
+//               Are you in the market for a specific product or service, but don't
+//               know where to start looking?InsideMarket is here to simplify your
+//               search and make your buying and service-seeking experience
+//               hassle-free.
+//             </Typography>
+//             <Box className="flex  mt-10  md:flex">
+//               <Link to="/" className=" ">
+//                 <button className="border-none mr-4">
+//                   <img src={appstore} alt="" />
+//                 </button>
+//               </Link>
+//               <Link to="/">
+//                 <button className="">
+//                   <img src={playstore} alt="" />
+//                 </button>
+//               </Link>
+//             </Box>
+//           </Box>
+
+//           <img className="lg:w-1/2" src={iphone1} alt="" />
+//         </Box>
+//       )}
+
+//       {/* TAB 2 Contents */}
+//       {currentTabIndex === 1 && (
+//         <Box className="container flex flex-wrap justify-center mx-auto  md:flex-row ">
+
+//           <Box sx={{ p: 0 }} className="lg:w-1/2">
+
+//             <Tabs
+//                 className=" container mx-auto mt-10  itesms-center lowercase "
+//                 value={currentTabIndex}
+//                 onChange={handleTabChange}
+//                 variant="scrollable"
+//                 scrollButtons="auto"
+//                 aria-label="scrollable auto tab example"
+//                 TabIndicatorProps={{
+//                     sx: {backgroundColor:  '#FF6A00', height: 4, textTransform: 'capitalize'}
+//                 }}
+//                 start>
+//                 <Tab className="text-xs capitalize" sx={{ textTransform: "none" }} label="For buyers" />
+//                 <Tab className="text-xs capitalize" sx={{ textTransform: "none" }} label="For Sellers and Service Providers" />
+//             </Tabs>
+
+//             <h2 className="max-w-xl text-2xl font-extrabold leading-none text-center lg:text-5xl lg:text-left lg:leading-tight pb-5">
+//               Connect with customers, increase your profits
+//             </h2>
+//             <Typography variant="p text-sm text-gray-500">
+//               InsideMarket is your go-to destination to find ready-to-buy,
+//               Ready-to-pay customers for your product or service. Install the
+//               app to get started!
+//             </Typography>
+//             <Box className="flex  mt-10  md:flex">
+//               <Link to="/" className=" ">
+//                 <button className="border-none mr-4">
+//                   <img src={appstore} alt="" />
+//                 </button>
+//               </Link>
+//               <Link to="/">
+//                 <button className="">
+//                   <img src={playstore} alt="" />
+//                 </button>
+//               </Link>
+//             </Box>
+//           </Box>
+
+//           <img className="lg:w-1/2 " src={iphone2} alt="" />
+//         </Box>
+//       )}
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
--- src/components/Howitworks.jsx
+++ src/components/Howitworks.jsx
@@ -13,18 +13,18 @@ const Howitworks = () => {
   };
 
   return (
-    <div id='howitworks' className="lg:p-20">
-      <div className="text-center ">
+    <Box id='howitworks' className="py-20">
+      <Box className="text-center ">
         <h1 className="font-extrabold text-4xl ">How it Works</h1>
-        <p className="text-gray-400 text-sm">
+        <Typography className="text-gray-400 text-sm">
           Think of it as twitter for e-commerce 
-        </p> 
-      </div> 
-      <div className="container mx-auto mt-7 flex flex-wrap-reverse  justify-center  ">
-      <div
+        </Typography> 
+      </Box> 
+      <Box className="container mx-auto mt-7 flex flex-wrap-reverse  justify-center xs:max-lg:px-3 ">
+      <Box
     
-      className="bloc-tabs grid mt-12   md:grid-cols-3 mx-auto flex-col justify-center border-white-500 border-t-2">
-            <div
+      className="bloc-tabs grid mt-12   md:grid-cols-3 mx-auto flex-col justify-center border-white-500 border-t-2 ">
+            <Box
             className={
                 toggleState === 1
                 ? "tabs md:relative bottom-[2px] xs:hover:bg-orange-100 border-orange-500 border-t-2  border-solid pt-4 text-center"
@@ -33,9 +33,9 @@ const Howitworks = () => {
             onMouseEnter={() => toggleTab(1)}
             >
             <h1 className="font-bold">Post your requests</h1>
-            <p className="font-thin text-xs text-gray-500">Post what you want to buy, whether in text, images or voice.</p>
-            </div>
-            <div
+            <Typography className="font-thin text-sm text-gray-500 max-w-md">Post what you want to buy, whether in text, images or voice.</Typography>
+            </Box>
+            <Box
             className={
                 toggleState === 2
                 ? "tabs relative bottom-[2px] xs:hover:bg-orange-100  border-orange-500 border-t-2 border-solid text-center pt-4"
@@ -44,11 +44,11 @@ const Howitworks = () => {
             onMouseEnter={() => toggleTab(2)}
             >
             <h1 className="font-bold">Sellers Respond with Offers</h1>
-            <p className="font-thin text-xs text-gray-500">
+            <Typography className="font-thin text-sm text-gray-500">
                 Within minutes, sellers Respond to your posts and give you offers.
-            </p>
-            </div>
-            <div
+            </Typography>
+            </Box>
+            <Box
             className={
                 toggleState === 3
                 ? "tabs relative bottom-[2px] xs:hover:bg-orange-100 border-t-2 border-t-orange-500 border-solid text-center pt-4"
@@ -57,39 +57,39 @@ const Howitworks = () => {
             onMouseEnter={() => toggleTab(3)}
             >
             <h1 className="font-bold">Choose your seller and buy</h1>
-            <p className="font-thin text-xs text-gray-500">
+            <Typography className="font-thin text-sm text-gray-500">
                 Connect with your chosen seller, chat, negotiate, and finalize deals
                 within the app.
-            </p>
-            </div>
-        </div>
+            </Typography>
+            </Box>
+        </Box>
 
-      <div className="content-tabs  items-center w-1/2 ">
-        <div
+      <Box className="content-tabs  items-center ">
+        <Box
           className={
             toggleState === 1
               ? "content active-content border-orange-500 border-t-6 text-center"
-              : "content text-center items-center "
+              : "content text-center items-center  "
           }
         >
           <img  src={iphone1} alt="" />
-        </div>
-        <div
+        </Box>
+        <Box
           className={toggleState === 2 ? "content active-content" : "content items-center "}
         >
           <img src={iphone2} alt="" />
-        </div>
+        </Box>
 
-        <div
+        <Box
           className={toggleState === 3 ? "content active-content" : "content items-center "}
         >
           <img src={iphone3} alt="" />
-        </div>
-      </div>
-    </div>
+        </Box>
+      </Box>
+    </Box>
 
 
-    </div>
+    </Box>
   )
 }
 
